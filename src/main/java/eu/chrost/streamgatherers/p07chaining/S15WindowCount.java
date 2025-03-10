e01a74ea2f908c6dd0eb6c1567405d288689a364
Marcin Chrost <mchrost82@gmail.com>
2025-03-11 00:18:27 +0100

    Chaining gatherers - window count

---
+++ src/main/java/eu/chrost/streamgatherers/p07chaining/S15WindowCount.java
@@ -0,0 +1,19 @@
+package eu.chrost.streamgatherers.p07chaining;
+
+import java.util.List;
+import java.util.stream.Gatherers;
+import java.util.stream.Stream;
+
+public class S15WindowCount {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        Stream<Integer> windowCount = numbers.stream()
+                .gather(Gatherers.windowFixed(3))
+                .gather(Gatherers.fold(() -> 0, (sum, _) -> sum + 1));
+        windowCount.forEach(System.out::println);
+
+        Stream<Integer> windowCountWithCompoundGatherer = numbers.stream()
+                .gather(Gatherers.windowFixed(3).andThen(Gatherers.fold(() -> 0, (sum, _) -> sum + 1)));
+        windowCountWithCompoundGatherer.forEach(System.out::println);
+    }
+}
