5db7da38dec143b930661973610922190b3eff85
Marcin Chrost <mchrost82@gmail.com>
2025-05-13 20:37:38 +0200

    Extracted pattern to variable

--- src/main/java/eu/chrost/streamgatherers/p03windowsliding/S05PatternMatching.java
+++ src/main/java/eu/chrost/streamgatherers/p03windowsliding/S05PatternMatching.java
@@ -6,13 +6,15 @@ import java.util.stream.Gatherers;
 record IndexedValue(int index, int value) {}
 
 public class S05PatternMatching {
+    private static final List<Integer> PATTERN = List.of(1, 0, 1);
+
     public static void main(String[] args) {
         List.of(1, 0, 1, 1, 0, 1, 0, 0, 1).stream()
                 .gather(Gatherers.scan(
                         () -> new IndexedValue(-1, 0),
                         (state, value) -> new IndexedValue(state.index() + 1, value)))
                 .gather(Gatherers.windowSliding(3))
-                .filter(window -> window.stream().map(IndexedValue::value).toList().equals(List.of(1, 0, 1)))
+                .filter(window -> window.stream().map(IndexedValue::value).toList().equals(PATTERN))
                 .forEach(pattern -> System.out.println("Pattern found at index: " + pattern.getFirst().index()));
     }
 }
