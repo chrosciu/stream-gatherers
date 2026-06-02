71d5396f9cc4d24a7fc7c6977d02894d9605518c
Marcin Chrost <marcin.chrost@allegro.com>
2026-06-02 12:53:27 +0300

    Clarified steps in pattern matching example

--- src/main/java/eu/chrost/streamgatherers/p04chaining/S04PatternMatchingWithIndex.java
+++ src/main/java/eu/chrost/streamgatherers/p04chaining/S04PatternMatchingWithIndex.java
@@ -3,7 +3,7 @@ package eu.chrost.streamgatherers.p04chaining;
 import java.util.List;
 import java.util.stream.Gatherers;
 
-record IndexedValue(int index, int value) {}
+record ItemWithIndex(int item, int index) {}
 
 public class S04PatternMatchingWithIndex {
     private static final List<Integer> PATTERN = List.of(1, 0, 1);
@@ -11,12 +11,14 @@ public class S04PatternMatchingWithIndex {
     public static void main(String[] args) {
         List.of(1, 0, 1, 1, 0, 1, 0, 0, 1).stream()
                 .gather(Gatherers.scan(
-                        () -> new IndexedValue(-1, 0),
-                        (state, value) ->
-                                new IndexedValue(state.index() + 1, value)))
+                        () -> new ItemWithIndex(0, -1),
+                        (state, item) ->
+                                new ItemWithIndex(item, state.index() + 1)))
+                //[item: 1, index: 0], [item: 0, index: 1], [item: 1, index: 2], [item: 1, index: 3]...
                 .gather(Gatherers.windowSliding(PATTERN.size()))
+                //[[1, 0], [0, 1], [1, 2]], [[0, 1], [1, 2], [1, 3]], ...
                 .filter(window ->
-                        window.stream().map(IndexedValue::value).toList().equals(PATTERN))
+                        window.stream().map(ItemWithIndex::item).toList().equals(PATTERN))
                 .forEach(pattern ->
                         System.out.println("Pattern " + PATTERN + " found at index: " + pattern.getFirst().index()));
     }
